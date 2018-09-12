208df32c57e539960b592f5e8defcd401f46e568
Walter <walterfrancoy@gmail.com>
2018-09-12 15:59:50 -0300

    segundo sprint
    modificacion de graficos

--- Galax/src/juego/Jugador.java
+++ Galax/src/juego/Jugador.java
@@ -9,9 +9,9 @@ public class Jugador extends Entidad{
 	public Jugador(int velocidad, int x, int y) {
 		super(velocidad, x, y);
 		
-		this.image[0] = new ImageIcon(this.getClass().getResource("/Galaxia/Nave.png"));
-		this.image[1] = new ImageIcon(this.getClass().getResource("/Galaxia/Nave.png"));
-		this.image[2] = new ImageIcon(this.getClass().getResource("/Galaxia/Nave.png"));
-		this.image[3] = new ImageIcon(this.getClass().getResource("/Galaxia/Nave.png"));
+		this.image[0] = new ImageIcon(this.getClass().getResource("/graficos/Nave.png"));
+		this.image[1] = new ImageIcon(this.getClass().getResource("/graficos/Nave.png"));
+		this.image[2] = new ImageIcon(this.getClass().getResource("/graficos/Nave.png"));
+		this.image[3] = new ImageIcon(this.getClass().getResource("/graficos/Nave.png"));
 	}
 }
--- Galax/src/juego/Malo.java
+++ Galax/src/juego/Malo.java
@@ -7,9 +7,9 @@ public class Malo extends Entidad{
 	public Malo(int velocidad, int x, int y) {
 		super(velocidad, x, y);
 		
-		this.image[0] = new ImageIcon(this.getClass().getResource("/Galaxia/Malo_1.png"));
-		this.image[1] = new ImageIcon(this.getClass().getResource("/Galaxia/Malo_2.png"));
-		this.image[2] = new ImageIcon(this.getClass().getResource("/Galaxia/Malo_3.png"));
-		this.image[3] = new ImageIcon(this.getClass().getResource("/Galaxia/Malo_4.png"));
+		this.image[0] = new ImageIcon(this.getClass().getResource("/graficos/Malo_1.png"));
+		this.image[1] = new ImageIcon(this.getClass().getResource("/graficos/Malo_2.png"));
+		this.image[2] = new ImageIcon(this.getClass().getResource("/graficos/Malo_3.png"));
+		this.image[3] = new ImageIcon(this.getClass().getResource("/graficos/Malo_4.png"));
 	}
 }
